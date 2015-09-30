851a10ff2d611e3e3dea718a25f3288274496938
zixian92 <zixian1992@hotmail.com>
2015-09-30 18:20:57 +0800

    Implements raw prototype of multithreaded downloader

--- components/downloader.py
+++ components/downloader.py
@@ -1,25 +1,34 @@
 import socket
-from dnscache import Dnscache
+from threading import Thread, BoundedSemaphore
 from httprequest import HttpRequests
 
-#url = "www.mizukinana.jp/blog/"
-url="a"
-req = HttpRequests.get(url, {"Connection": "close", "User-Agent": "game-price-crawler"})
-#req.setHeader("Connection", "close")
-print req
-dnsproxy = Dnscache()
-dnsproxy.addMapping('a', '1');
-print dnsproxy.getAddressForHostname('a')
+# Defines the downloader component
+class Downloader(object):
+	NUMTHREAD_DEFAULT = 4
+	def __init__(self, maxConcurrentRequests = NUMTHREAD_DEFAULT):
+		self.numThreads = maxConcurrentRequests
+		self.resourcePool = BoundedSemaphore(self.numThreads)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-try:
-	s.connect((req.getHeader("Host"), 80))==0
-	s.send(str(req))
-	res = s.recv(2048)
-	while res!="":
-		print res
-		res = s.recv(2048)
-except:
-	print "Unable to connect to %s" % (url)
-print "closing socket"
-s.close()
+	# Downloads the HTML file at the given URL and writes to file with same name as URL.
+	# Blocks if there are more than the specified threshold concurrent requests running.
+	def download(self, url):
+		self.resourcePool.acquire()	# Blocking by default
+		Thread(target=self.privateDownload, args=(url,)).start()
+
+	# Private
+	def privateDownload(self, url):
+		req = HttpRequests.get(url, {"Connection": "close", "User-Agent": "game-price-crawler"})
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			s.connect((req.getHeader("Host"), 80))
+			f = open(url, 'w')
+			s.send(str(req))
+			res = s.recv(2048)
+			while res!="":
+				f.write(res)
+				res = s.recv(2048)
+			f.close()
+		except:
+			print "Unable to fetch resource %s" % (url)
+		s.close()
+		self.resourcePool.release()
---
+++ crawler.py
@@ -0,0 +1,8 @@
+from components.downloader import Downloader
+
+d = Downloader()
+d.download("www.google.com")
+d.download("www.mizukinana.jp")
+d.download("www.facebook.com")
+d.download("www.comp.nus.edu.sg")
+d.download("www.hotmail.com")
