e03f390fdfffbcd987038f260bae56f5910efcac
sharminelow <slxmlow@gmail.com>
2015-10-07 05:06:48 +0800

    refactor Classifier methods

--- parser/Classifier.py
+++ parser/Classifier.py
@@ -16,11 +16,9 @@ class Classifier(object):
     if "Rakuten" in title:
       return self.getPriceRakuten(page)
     elif "GameTrader" in title:
-      self.getPriceGametrader(page)
+      return self.getPriceGametrader(page)
 
   # if its Rakuten's product page
-  # http://www.rakuten.com.sg/shop/shopitree/product/045496741723/?l-id=sg_search_product_2
-  # get name, price, platform
   def getPriceRakuten(self, page):
     # determine if its a product page
     if len(page.find_all(attrs={"property":"og:type"})) == 0:
@@ -29,46 +27,88 @@ class Classifier(object):
 
     # looking into javascript contents
     scriptContent = str(page.script.string)
-    m = re.search(r"'prod_price': ((?:\d|\.)*)", scriptContent)
-    self.data['price'] = m.group(1)
-
-    startPos = scriptContent.find('prod_name')
-    startPos = startPos + 13
-    endPos = scriptContent.find('\"', startPos)
-    name = scriptContent[startPos:endPos]
-    self.data['name'] = name
-
-    m = Classifier.CONST_PLATFORMS.search(name)
-    if m is not None:
-      self.data['platform'] = m.group(0) # platform
+    self.data['price'] = self._rakutenGetPrice(scriptContent)
+    self.data['name'] = self._rakutenGetName(scriptContent)
+    self.data['platform'] = self._rakutenGetPlatform(self.data['name']) # platform
 
     print self.data
     return self.data
 
   def getPriceGametrader(self, page):
     # determine if product page
+    # try:
     gameInfo = page.select('table[cellpadding="2"] > tr')
-    try:
-      if gameInfo[1].td.div.span.strong.string == "Title":
-        self.data['name'] = gameInfo[1].contents[3].span.contents[0].strip()
-        shortenedInfo = gameInfo[2:5]
-        for (count, info) in enumerate(shortenedInfo):
-          infoHtml = info.contents # Price: 320.00 or Plat: Xbox
-          if count == 0:
-            print infoHtml[3].contents
-            self.data['platform'] = infoHtml[3].string.strip()
+    if self._gametraderGetTitleTag(gameInfo) == "Title":
+      self.data['name'] = self._gametraderGetName(gameInfo)
+      shortenedInfoList = gameInfo[2:5]
 
-          elif count == 1:
-            price = next(infoHtml[3].strings).strip()
-            self.data['price'] = price[2:] # remove dollar sign
+      for (count, info) in enumerate(shortenedInfoList):
+        infoHtml = info.contents # Price: 320.00 or Plat: Xbox
 
-          elif count == 2:
-            self.data['status'] = infoHtml[3].string.strip()
-      else:
-        print "else This is not a Gametrader product page - "
-    except:
-     print "This is not a Gametrader product page - "
-     # print sys.exc_info()[0]
+        if count == 0:
+          self.data['platform'] = self._gametraderGetPlatOrStatus(infoHtml)
+
+        elif count == 1:
+          self.data['price'] = self._gametraderGetPrice(infoHtml)[2:] # remove dollar sign
+
+        elif count == 2:
+          self.data['status'] = self._gametraderGetPlatOrStatus(infoHtml)
+
+    else:
+      print "else This is not a Gametrader product page - "
+    # except:
+    #  print "This is not a Gametrader product page - "
+    #  print sys.exc_info()[0]
     print self.data
     return self.data
 
+  @staticmethod
+  def _rakutenGetPrice(scriptContent):
+    m = re.search(r"'prod_price': ((?:\d|\.)*)", scriptContent)
+    if m is not None:
+      return m.group(1)
+    else:
+      return None
+
+  @staticmethod
+  def _rakutenGetName(scriptContent):
+    startPos = scriptContent.find('prod_name')
+    startPos = startPos + 13
+    endPos = scriptContent.find('\"', startPos)
+    return scriptContent[startPos:endPos]
+
+  @staticmethod
+  def _rakutenGetPlatform(name):
+    m = Classifier.CONST_PLATFORMS.search(name)
+    if m is not None:
+      return m.group(0)
+    else:
+      return None
+
+  @staticmethod
+  def _gametraderGetTitleTag(gameInfo):
+    try:
+      return gameInfo[1].td.div.span.strong.string
+    except:
+      return None
+
+  @staticmethod
+  def _gametraderGetName(gameInfo):
+    try:
+      return gameInfo[1].contents[3].span.contents[0].strip()
+    except:
+      return None
+
+  @staticmethod
+  def _gametraderGetPlatOrStatus(infoHtml):
+    try:
+      return infoHtml[3].string.strip()
+    except:
+      return None
+
+  @staticmethod
+  def _gametraderGetPrice(infoHtml):
+    try:
+      return next(infoHtml[3].strings).strip()
+    except:
+      return None
