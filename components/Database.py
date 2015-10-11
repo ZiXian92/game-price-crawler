cdbe6257fbf06ab914966728c1c1d3233b34b946
MunAw <jasontanma@gmail.com>
2015-10-11 14:44:18 +0800

    Added simple connection to mysql db and query + insert

---
+++ components/Database.py
@@ -0,0 +1,79 @@
+import mysql.connector
+from datetime import date, datetime
+from mysql.connector import errorcode
+
+class Database:
+  def __init__(self):
+    self.connection = mysql.connector.connect(
+                              user='', 
+                              password='',
+                              host='localhost',
+                              database='test'
+                      )
+
+    cursor = self.connection.cursor()
+
+    table = (
+      "CREATE TABLE `pricelist` ("
+      "   id Integer AUTO_INCREMENT PRIMARY KEY,"
+      "   name varchar(512) NOT NULL,"
+      "   url  varchar(1024) NOT NULL,"
+      "   price float NOT NULL,"
+      "   lastUpdate datetime NOT NULL,"
+      "   createdAt datetime NOT NULL,"
+      "   updatedAt datetime NOT NULL"
+      ")"
+    )
+
+    try:
+      cursor.execute(table)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+    cursor.close()
+
+  def queryByName(self, name):
+    cursor = self.connection.cursor()
+
+    cursor.execute( "SELECT * FROM pricelist "
+                    "WHERE name LIKE '%" + name + "%'")
+    
+    arr = []
+
+    for(id, name, url, price, lastUpdate, createdAt, updatedAt) in cursor:
+      entry = {
+        "id": id,
+        "name": name,
+        "url": url,
+        "price": price,
+        "lastUpdate": lastUpdate.strftime("%Y-%m-%d %H:%M:%S"),
+        "createdAt": createdAt.strftime("%Y-%m-%d %H:%M:%S"),
+        "updatedAt": updatedAt.strftime("%Y-%m-%d %H:%M:%S")
+      }
+      arr.append(entry)
+
+    cursor.close()
+    return arr
+
+  def insertURL(self, name, price, url, lastUpdate):
+    cursor = self.connection.cursor()
+
+    addEntry = ("INSERT INTO pricelist "
+                "(name, url, price, lastUpdate, createdAt, updatedAt) "
+                "VALUES (%s, %s, %s, %s, %s, %s)")
+
+    data = (name, url, price, lastUpdate, datetime.now(), datetime.now())
+    
+    cursor.execute(addEntry, data)
+
+    self.connection.commit()
+    cursor.close()
+
+# db = Database()
+# db.insertURL("Mario Cart", 25.03, "http://test/mario1", "2015/10/10 10:10")
+# print db.queryByName("Mari")
