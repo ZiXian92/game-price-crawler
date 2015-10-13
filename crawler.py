dc354a01657cf9e35f70ab3573197681343cb02f
sharminelow <slxmlow@gmail.com>
2015-10-13 23:19:22 +0800

    Remove qisahn domain links

--- crawler.py
+++ crawler.py
@@ -4,19 +4,13 @@ class Crawler(object):
   if __name__ == '__main__':
     d = Downloader()
 
-    d.download('http://qisahn.com/')
-    d.download('http://qisahn.com/products/metal-gear-solid-v-the-phantom-pain-2')
-    d.download('http://qisahn.com/buyback')
-    d.download('http://qisahn.com/pg/nintendo-3ds-new-3ds-xl-games-pre-order')
-    d.download('http://qisahn.com/products/resident-evil-archives-resident-evil-zero')
-    d.download('http://qisahn.com/products/chibi-robo-zip-lash-1')
-
-    """
     d.download("https://www.gametrader.sg/profile.php?nick=sirius&platform=PS3")
     d.download("https://carousell.com/p/20557583/")
     d.download("https://carousell.com/")
     d.download("https://www.gametrader.sg/game_pg.php?post_id=140275&title=FIFA+16&platform=PS3&seller=sirius")
     d.download("https://www.gametrader.sg/game_pg.php?post_id=140452&title=Jtag+xbox+360+Slim&platform=Xbox%20360&seller=seechen")
+
+    """
     d.download("www.google.com")
     d.download("www.mizukinana.jp")
     d.download("www.facebook.com")
@@ -28,3 +22,4 @@ class Crawler(object):
     d.download('http://qisahn.com/products/lego-batman-2-dc-super-heroes-1')
     d.download('http://www.funzsquare.com/games-used-c-135_158.html')
     """
+
