62582cef5c82d4d60f2e6019a4d6f3c06847db4e
sharminelow <slxmlow@gmail.com>
2015-11-05 01:57:13 +0800

    changed all tabs to spaces

--- components/downloader.py
+++ components/downloader.py
@@ -9,121 +9,121 @@ from datetime import datetime
 class Downloader(object):
     NUMTHREAD_DEFAULT = 4
 
-	# Limit maximum number of threads
-	# Semaphore to enforce upper limit
-	# Restrict result queue size to prevent out-of-memory
+    # Limit maximum number of threads
+    # Semaphore to enforce upper limit
+    # Restrict result queue size to prevent out-of-memory
     def __init__(self, maxConcurrentRequests = NUMTHREAD_DEFAULT):
-		self.numThreads = maxConcurrentRequests
-		self.resourcePool = BoundedSemaphore(self.numThreads)
-		self.resQueue = Queue(100000)
+        self.numThreads = maxConcurrentRequests
+        self.resourcePool = BoundedSemaphore(self.numThreads)
+        self.resQueue = Queue(100000)
 
-	# Dispatches a separate thread to download file.
-	# Downloads the HTML file at the given URL and writes to file with same name as URL.
-	# Blocks if there are more than the specified threshold concurrent requests running.
+    # Dispatches a separate thread to download file.
+    # Downloads the HTML file at the given URL and writes to file with same name as URL.
+    # Blocks if there are more than the specified threshold concurrent requests running.
     def download(self, url):
-		print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Trying to get thread"
-		self.resourcePool.acquire()	# Blocking by default
-		print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Successfully acquired thread"
-		Thread(target=self.privateDownload, args=(url,)).start()
+        print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Trying to get thread"
+        self.resourcePool.acquire()	# Blocking by default
+        print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Successfully acquired thread"
+        Thread(target=self.privateDownload, args=(url,)).start()
 
-	# Returns a tuple (url, htmlString, RTT)
-	# Blocks until there is a result
+    # Returns a tuple (url, htmlString, RTT)
+    # Blocks until there is a result
     def getResult(self):
-		return self.resQueue.get(True)
+        return self.resQueue.get(True)
 
-	# Private
+    # Private
     def privateDownload(self, url):
-		if url==None:
-			return
+        if url==None:
+            return
 
-		# Chop off any trailing CRLF or LF
-		crIndex = url.find("\r")
-		lfIndex = url.find("\n")
-		if (crIndex==-1) != (lfIndex==-1):
-			url = url[0:max(crIndex, lfIndex)]
-		elif crIndex!=-1:
-			url = url[0:min(crIndex, lfIndex)]
+        # Chop off any trailing CRLF or LF
+        crIndex = url.find("\r")
+        lfIndex = url.find("\n")
+        if (crIndex==-1) != (lfIndex==-1):
+            url = url[0:max(crIndex, lfIndex)]
+        elif crIndex!=-1:
+            url = url[0:min(crIndex, lfIndex)]
 
-		# Form HTTP request
-		req = HttpRequests.get(url, {"Connection": "close", "User-Agent": "game-price-crawler", "Accept": "text/html"})
+        # Form HTTP request
+        req = HttpRequests.get(url, {"Connection": "close", "User-Agent": "game-price-crawler", "Accept": "text/html"})
 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		# Use TLS socket if is HTTPS
-		try:
-			if req.isSecureProtocol():
-				s = ssl.wrap_socket(s)
-				s.connect((req.getHeader("Host"), 443))
-				s.do_handshake()
-			else:
-				s.connect((req.getHeader("Host"), 80))
+        # Use TLS socket if is HTTPS
+        try:
+            if req.isSecureProtocol():
+                s = ssl.wrap_socket(s)
+                s.connect((req.getHeader("Host"), 443))
+                s.do_handshake()
+            else:
+                s.connect((req.getHeader("Host"), 80))
 
-			print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: Sending request" % (url)
-			start = time.time()
+            print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: Sending request" % (url)
+            start = time.time()
 
-			# Send request
-			s.send(str(req))
+            # Send request
+            s.send(str(req))
 
-			# Use temporary file to buffer socket input
-			fp = s.makefile('r', 2048)
-			res = ""
-			line = fp.readline()
-			end = time.time()
-			print end-start
+            # Use temporary file to buffer socket input
+            fp = s.makefile('r', 2048)
+            res = ""
+            line = fp.readline()
+            end = time.time()
+            print end-start
 
-			# Read only the response header
-			while line!="\r\n" and line!="":
-				res+=line
-				line = fp.readline()
-			res+=line
+            # Read only the response header
+            while line!="\r\n" and line!="":
+                res+=line
+                line = fp.readline()
+            res+=line
 
-			# Convert header string into object for easy access of header fields
-			headers = Downloader.convertToHeaderObject(res)
-			print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: %d %s" % (url, headers["statusCode"], headers["statusMessage"])
+            # Convert header string into object for easy access of header fields
+            headers = Downloader.convertToHeaderObject(res)
+            print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: %d %s" % (url, headers["statusCode"], headers["statusMessage"])
 
-			# Status OK, proceed to download
-			if headers["statusCode"]==200:
-				res = fp.read()
-				try:
-					self.resQueue.put((url, res, int((end-start)*1000)), True, 0.5)
-				except:
-					print datetime.now().strftime("%d/%m/%Y %H:%M%S") + ": Result queue full, dropping "+url
+            # Status OK, proceed to download
+            if headers["statusCode"]==200:
+                res = fp.read()
+                try:
+                    self.resQueue.put((url, res, int((end-start)*1000)), True, 0.5)
+                except:
+                    print datetime.now().strftime("%d/%m/%Y %H:%M%S") + ": Result queue full, dropping "+url
 
-			# A redirect response, follow the redirect link
-			elif headers.get("Location")!=None:
-				print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: Re: directing to %s" % (url, headers["Location"])
-				fp.close()
-				s.close()
-				self.privateDownload(headers["Location"])
-				return
-			fp.close()
-		except Exception as e:
-			print str(e)
-			print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Unable to fetch resource %s" % (url)
+            # A redirect response, follow the redirect link
+            elif headers.get("Location")!=None:
+                print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": %s: Re: directing to %s" % (url, headers["Location"])
+                fp.close()
+                s.close()
+                self.privateDownload(headers["Location"])
+                return
+            fp.close()
+        except Exception as e:
+            print str(e)
+            print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Unable to fetch resource %s" % (url)
 
-		# Close socket and allow other threads to be spawned
-		s.close()
-		self.resourcePool.release()
-		print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Thread released by " + url
+        # Close socket and allow other threads to be spawned
+        s.close()
+        self.resourcePool.release()
+        print datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": Thread released by " + url
 
-	# s must be a properly formatted HTTP response header string,
-	# each line ending with CRLF and the header ending with CRLF after the last line.
+    # s must be a properly formatted HTTP response header string,
+    # each line ending with CRLF and the header ending with CRLF after the last line.
     @staticmethod
     def convertToHeaderObject(s):
-		obj = {}
-		sidx = s.find(" ")+1
-		eidx = s.find(" ", sidx)
-		obj['statusCode'] = int(s[sidx:eidx])
-		sidx = eidx+1
-		eidx = s.find("\r\n", sidx)
-		obj['statusMessage'] = s[sidx:eidx]
-		sidx = eidx+2
-		eidx = s.find("\r\n", sidx)
-		while eidx!=sidx:
-			l = s[sidx:eidx]
-			header = l[:l.find(":")]
-			value = l[l.find(": ")+2:]
-			obj[header] = value
-			sidx = eidx+2
-			eidx = s.find("\r\n", sidx)
-		return obj
+        obj = {}
+        sidx = s.find(" ")+1
+        eidx = s.find(" ", sidx)
+        obj['statusCode'] = int(s[sidx:eidx])
+        sidx = eidx+1
+        eidx = s.find("\r\n", sidx)
+        obj['statusMessage'] = s[sidx:eidx]
+        sidx = eidx+2
+        eidx = s.find("\r\n", sidx)
+        while eidx!=sidx:
+            l = s[sidx:eidx]
+            header = l[:l.find(":")]
+            value = l[l.find(": ")+2:]
+            obj[header] = value
+            sidx = eidx+2
+            eidx = s.find("\r\n", sidx)
+        return obj
--- components/httprequest.py
+++ components/httprequest.py
@@ -3,86 +3,86 @@ from abc import ABCMeta, abstractmethod
 
 # Defines a factory for HTTP requests
 class HttpRequests(object):
-	# url: The URL string. Cannot be None.
-	# headers: Dictionary with string as key and value
-	@staticmethod
-	def get(url, headers={}):
-		return HttpGet(url, headers)
+    # url: The URL string. Cannot be None.
+    # headers: Dictionary with string as key and value
+    @staticmethod
+    def get(url, headers={}):
+        return HttpGet(url, headers)
 
 # Defines an abstract Http request object
 class HttpRequest:
-	__metaclass__ = ABCMeta
-	protocol = "HTTP/1.1"
+    __metaclass__ = ABCMeta
+    protocol = "HTTP/1.1"
 
-	# url: The URL string
-	# headers(Optional): Dictionary of header name string to header value string
-	def __init__(self, url, headers={}):
-		# Construct bare essential of HTTP request
-		if url==None:
-			self.uri = headers["Host"] = ""
-			self.isHttps = False
-		else:
-			self.uri = HttpRequest.getUriFromUrl(url)
-			headers["Host"] = HttpRequest.getHostFromUrl(url)
-			self.isHttps = url.startswith("https://")
-		headers["User-Agent"] = "zxhttprequest" if headers.get("User-Agent")==None else headers["User-Agent"]
-		headers["Accept"] = "*/*" if headers.get("Accept")==None else headers["Accept"]
-		self.headers = headers
+    # url: The URL string
+    # headers(Optional): Dictionary of header name string to header value string
+    def __init__(self, url, headers={}):
+        # Construct bare essential of HTTP request
+        if url==None:
+            self.uri = headers["Host"] = ""
+            self.isHttps = False
+        else:
+            self.uri = HttpRequest.getUriFromUrl(url)
+            headers["Host"] = HttpRequest.getHostFromUrl(url)
+            self.isHttps = url.startswith("https://")
+        headers["User-Agent"] = "zxhttprequest" if headers.get("User-Agent")==None else headers["User-Agent"]
+        headers["Accept"] = "*/*" if headers.get("Accept")==None else headers["Accept"]
+        self.headers = headers
 
-	# Gets the host part of the URL
-	@staticmethod
-	def getHostFromUrl(url):
-		startIndex = url.find("//")
-		startIndex = 0 if startIndex == -1 else startIndex+2
-		endIndex = url.find("/", startIndex)
-		endIndex = len(url) if endIndex == -1 else endIndex
-		queryIndex = url.find("?", startIndex)
-		queryIndex = endIndex if queryIndex == -1 else queryIndex
-		endIndex = queryIndex if queryIndex < endIndex else endIndex
-		return url[startIndex: endIndex]
+    # Gets the host part of the URL
+    @staticmethod
+    def getHostFromUrl(url):
+        startIndex = url.find("//")
+        startIndex = 0 if startIndex == -1 else startIndex+2
+        endIndex = url.find("/", startIndex)
+        endIndex = len(url) if endIndex == -1 else endIndex
+        queryIndex = url.find("?", startIndex)
+        queryIndex = endIndex if queryIndex == -1 else queryIndex
+        endIndex = queryIndex if queryIndex < endIndex else endIndex
+        return url[startIndex: endIndex]
 
-	# Gets the resource path from the given URL
-	@staticmethod
-	def getUriFromUrl(url):
-		startIndex = url.find("//")
-		startIndex = 0 if startIndex == -1 else startIndex+2
-		startIndex = url.find("/", startIndex)
-		if startIndex!=-1:
-			return url[startIndex:]
-		else:
-			queryIndex = url.find("?")
-			return "/" if queryIndex==-1 else "/" + url[queryIndex:]
+    # Gets the resource path from the given URL
+    @staticmethod
+    def getUriFromUrl(url):
+        startIndex = url.find("//")
+        startIndex = 0 if startIndex == -1 else startIndex+2
+        startIndex = url.find("/", startIndex)
+        if startIndex!=-1:
+            return url[startIndex:]
+        else:
+            queryIndex = url.find("?")
+            return "/" if queryIndex==-1 else "/" + url[queryIndex:]
 
-	# Returns the value set for the given header.
-	# Returns None if no such header exists.
-	def getHeader(self, headername):
-		return self.headers.get(headername)
+    # Returns the value set for the given header.
+    # Returns None if no such header exists.
+    def getHeader(self, headername):
+        return self.headers.get(headername)
 
-	# Returns whether this request is HTTPS
-	def isSecureProtocol(self):
-		return self.isHttps
+    # Returns whether this request is HTTPS
+    def isSecureProtocol(self):
+        return self.isHttps
 
-	# Returns the resource path of this request
-	def getPath(self):
-		return self.uri
+    # Returns the resource path of this request
+    def getPath(self):
+        return self.uri
 
-	# header: Header name string.
-	# value: Header value string
-	def setHeader(self, header, value):
-		self.headers[header] = value
+    # header: Header name string.
+    # value: Header value string
+    def setHeader(self, header, value):
+        self.headers[header] = value
 
-	# Convert to string form for sending over socket
-	def __str__(self):
-		reqStr = self.method + " " + self.uri + " " + HttpRequest.protocol + "\r\n"
-		for header, value in self.headers.iteritems():
-			reqStr+=header + ": " + value + "\r\n"
-		reqStr+="\r\n"
-		return reqStr
+    # Convert to string form for sending over socket
+    def __str__(self):
+        reqStr = self.method + " " + self.uri + " " + HttpRequest.protocol + "\r\n"
+        for header, value in self.headers.iteritems():
+            reqStr+=header + ": " + value + "\r\n"
+        reqStr+="\r\n"
+        return reqStr
 
 # Defines a HTTP GET request
 class HttpGet(HttpRequest):
-	def __init__(self, url, headers={}):
-		super(HttpGet, self).__init__(url, headers)
-		self.method = "GET"
+    def __init__(self, url, headers={}):
+        super(HttpGet, self).__init__(url, headers)
+        self.method = "GET"
 
 
