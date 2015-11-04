fc096af143d01de4d2b065506aab602170b1c7b7
zixian92 <zixian1992@hotmail.com>
2015-11-04 22:03:13 +0800

    Adds code to query by name

--- frontend/scripts/query.php
+++ frontend/scripts/query.php
@@ -4,6 +4,15 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
+$where = " WHERE ";
+$and = " AND ";
+$or = " OR ";
+$sql = "SELECT name, price, platform, cond, url, rtt FROM pricelist ";
+$nameFilter = " name like '%(?)%'";
+$conditionFilter = " cond like '%(?)%'";
+$priceFilter = " price>=(?) AND price<=(?) ";
+$platformFilter = " platform='(?)' ";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,19 +21,34 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name, price, platform, cond, url, rtt FROM pricelist";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    $res = array();
-    while($row = $result->fetch_assoc()) {
-        array_push($res, $row);
-    }
-    echo json_encode($res);
-
-} else {
-    echo "0 results";
+$query = $sql;
+if($_GET["searchTerm"]!=null && $_GET["searchTerm"]!=""){
+	$query+=$nameFilter;
+	$stmt = $conn->prepare($query);
+	$stmt->bind_param('s', $_GET["searchTerm"]);
+} else{
+	$stmt = $conn->prepare($query);
 }
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+$res = array();
+while($row = $result->fetch_assoc()){
+	array_push($res, $row);
+}
+
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     $res = array();
+//     while($row = $result->fetch_assoc()) {
+//         array_push($res, $row);
+//     }
+//     echo json_encode($res);
+
+// } else {
+//     echo "0 results";
+// }
+$stmt->close();
 $conn->close();
 ?>
